a65dcf468279be9f103b171b6f7af8ba545f1e80
VegadelaLyra <lyradeperseo@gmail.com>
2022-12-04 19:08:49 -0500

    Dross Landing Page working. Ready to push to github pages server.

--- src/assets/main.js
+++ src/assets/main.js
@@ -33,11 +33,11 @@ async function fetchData(urlApi) {
         let view =  `
             ${videos.items.map(video => `
                 <div class="group relative">
-                    <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <div class=" w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-90 lg:aspect-none">
                         <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
                     </div>
                     <div class="mt-4 flex justify-between">
-                        <h3 class="text-sm text-gray-700">
+                        <h3 class="text-sm text-gray-100">
                             <span aria-hidden="true" class="absolute inset-0"></span>
                             ${video.snippet.title}
                         </h3>
@@ -45,6 +45,7 @@ async function fetchData(urlApi) {
                 </div>
             `).slice(0, 4).join('')
         }`
+        content.innerHTML = view
     } catch (error) {
         console.log(error)
     } finally {
